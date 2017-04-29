5b82024f372c7173960794ff9115ffb6d69d195b
Pankaj Baranwal <pankajisadeveloper@gmail.com>
2017-04-29 11:09:57 +0530

    Added crowd-sourced data submitted API

--- models/user_profile.js
+++ models/user_profile.js
@@ -48,28 +48,29 @@ function Users() {
         .then(function(db){
             return db.collection('user_profiles')
             .then(function(col){
-//-----------Check for existing data--------------------------------------------------------
+//-----------Check for existing user--------------------------------------------------------
               return col.find( { "user_id": req.body.user_id }).count()
                 .then(function(value) {
-                    if(value==0)
-                    {
-                          db.close().then(console.log('success'));
-                          res.json({
-                              success: true,
-                              message: 'Data not available!!',
-                          });
-                    }
-//------------Fetch data if available------------------------------------------------------------------------
-                    else
-                      {
-                       
-                                  db.close().then(console.log('success'));
-                                  res.json({
-                                      success: true,
-                                      user_details: value,
-                                  });
 
-                      }
+                            return col.update({user_id : req.body.user_id} ,
+                                              {
+                                                $set:{
+                                                  sighting_data: req.body.sighting_data
+                                                }
+                                              },
+                                              {
+                                                upsert:true
+                                              }
+                                            )
+                                            .then(function(result) {
+                                                db.close().then(console.log('success'));
+                                                res.json({
+                                                    success: true,
+                                                    message: 'Thank you for helping save cranes!!',
+                                                });
+
+                                              }).fail(function(err) {console.log(err);});
+
                 })
               }).fail(function(err) {console.log(err);});
         })
