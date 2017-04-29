57ac8ab2094b2a440a3d683d6d9f6b4b3ead48f1
Pankaj Baranwal <pankajisadeveloper@gmail.com>
2017-04-29 09:27:43 +0530

    API for fetching user profiles added

---
+++ models/user_profile.js
@@ -0,0 +1,81 @@
+var connection = require('../connection');
+var express=require('express');
+var jwt    = require('jsonwebtoken');
+var user='myuser';
+var level=null;
+function Users() {
+  this.get_profile = function(req, res)
+  {
+    connection.conn.connect(connection.url)
+        .then(function(db){
+            return db.collection('user_profiles')
+            .then(function(col){
+//-----------Check for existing data--------------------------------------------------------
+              return col.find( { "user_id": req.body.user_id }).count()
+                .then(function(value) {
+                    if(value==0)
+                    {
+                          db.close().then(console.log('success'));
+                          res.json({
+                              success: true,
+                              message: 'Data not available!!',
+                          });
+                    }
+//------------Fetch data if available------------------------------------------------------------------------
+                    else
+                      {
+                       
+                                  db.close().then(console.log('success'));
+                                  res.json({
+                                      success: true,
+                                      user_details: value,
+                                  });
+
+                      }
+                })
+              }).fail(function(err) {console.log(err);});
+        })
+        .fail(function(err) {console.log(err);});
+    //-------------------------------------------------------------------------------------------
+ };
+
+ //Add user data----------------------------------------------------------------------------------
+
+
+   this.add_data = function(req, res)
+  {
+    connection.conn.connect(connection.url)
+        .then(function(db){
+            return db.collection('user_profiles')
+            .then(function(col){
+//-----------Check for existing data--------------------------------------------------------
+              return col.find( { "user_id": req.body.user_id }).count()
+                .then(function(value) {
+                    if(value==0)
+                    {
+                          db.close().then(console.log('success'));
+                          res.json({
+                              success: true,
+                              message: 'Data not available!!',
+                          });
+                    }
+//------------Fetch data if available------------------------------------------------------------------------
+                    else
+                      {
+                       
+                                  db.close().then(console.log('success'));
+                                  res.json({
+                                      success: true,
+                                      user_details: value,
+                                  });
+
+                      }
+                })
+              }).fail(function(err) {console.log(err);});
+        })
+        .fail(function(err) {console.log(err);});
+    //-------------------------------------------------------------------------------------------
+ };
+}
+
+module.exports = new Users();
